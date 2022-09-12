f43aa7071ba170a320902d5307785ba9543082f1
P1tirim <andrey.orishev@mail.ru>
2022-09-12 16:53:02 +0300

    v0.01042
    Remake definition of the cells which player can move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,42 +18,78 @@ public class Player : MonoBehaviour
 
         PlayerUnit player = new PlayerUnit(name, this.gameObject);
 
-        ChangeColorCell();
+        CellInMoveList();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //Change color of cell, which player can move on it
-    void ChangeColorCell()
+    //Change color of cell, which player can move on it and add in move list. Breadth-first search
+    void CellInMoveList()
     {
         //clear list of cells
-        cellsMoveList.Clear();
+        CleanCells();
 
         //get coordinates cell which player stay it
         string[] xy = transform.parent.name.Split(new char[] { ' ' });
         int x = int.Parse(xy[1]);
         int y = int.Parse(xy[0]);
-        for (int i = 0; i < cellsParent.transform.childCount; i++)
+
+        //Add the cell on which the player is located
+        cellsMoveList.Add(transform.parent.gameObject);
+        int countCells = cellsMoveList.Count;
+
+        //Breadth-first search
+        //count of steps for moving to cell
+        for (int step = 1; step <= moveSpeed; step++)
         {
-            GameObject cell = cellsParent.transform.GetChild(i).gameObject;
-            cell.GetComponent<SpriteRenderer>().color = Color.gray;
-
-            //get coordintates cell in cycle
-            string[] xy_ = cell.name.Split(new char[] { ' ' });
-            int x_ = int.Parse(xy_[1]);
-            int y_ = int.Parse(xy_[0]);
-
-            //checking whether the player can move into this cell
-            if ((Mathf.Abs(x - x_) + Mathf.Abs(y - y_)) <= moveSpeed && transform.parent.gameObject != cell)
+            countCells = cellsMoveList.Count;
+            for(int i = 0; i <countCells; i++)
             {
-                cell.GetComponent<SpriteRenderer>().color = Color.white;
-                cellsMoveList.Add(cell);
+                xy = cellsMoveList[i].name.Split(new char[] { ' ' });
+                x = int.Parse(xy[1]);
+                y = int.Parse(xy[0]);
+
+                for (int j = 0; j < cellsParent.transform.childCount; j++)
+                {
+                    GameObject cell = cellsParent.transform.GetChild(j).gameObject;
+
+                    bool skip = false;
+                    //skip iteration if on this cell stay enemy
+                    for(int ch = 0; ch < cell.transform.childCount; ch++)
+                    {
+                        if (cell.transform.GetChild(ch).tag == "Enemy") skip = true;
+                    }
+
+                    //skip iteration if this cell already in the list
+                    for(int c = 0; c < cellsMoveList.Count; c++)
+                    {
+                        if (cell == cellsMoveList[c]) skip = true;
+                    }
+
+                    if (skip) continue;
+
+                    string[] xy_ = cell.name.Split(new char[] { ' ' });
+                    int x_ = int.Parse(xy_[1]);
+                    int y_ = int.Parse(xy_[0]);
+
+                    if ((Mathf.Abs(x - x_) + Mathf.Abs(y - y_)) == 1)
+                    {
+                        cellsMoveList.Add(cell);
+                        cell.GetComponent<SpriteRenderer>().color = Color.white;
+                    }
+                }
             }
         }
+        //Remove the cell on which player stay
+        cellsMoveList.RemoveAt(0);
+    }
+
+    //Make all cells gray and clean the list
+    void CleanCells()
+    {
+        for(int i = 0; i < cellsParent.transform.childCount; i++)
+        {
+            cellsParent.transform.GetChild(i).GetComponent<SpriteRenderer>().color = Color.gray;
+        }
+        cellsMoveList.Clear();
     }
 
     //Checking, is can player move in this cell
@@ -75,7 +111,7 @@ public class Player : MonoBehaviour
         pos.y += 0.3f;
         transform.position = pos;
         transform.parent = cell.transform;
-        ChangeColorCell();
+        CellInMoveList();
     }
 
     class PlayerUnit
